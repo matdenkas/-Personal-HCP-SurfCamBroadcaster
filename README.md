866be80cc1e9d0b37489106348edcba9f7012510
Kastl <mkastl@islander.tamucc.edu>
2022-08-22 14:05:19 -0500

    Fetching video stream to backend

--- README.md
+++ README.md
@@ -8,11 +8,16 @@ A node/react webserver used to brodcast the surfcam at HCP  Horace Calwell Pier 
 ## Getting Started
 
 ### Dependencies
-* npm i express
+* npm i express //For web application
+* npm i node-rtsp-stream //For reading rtsp streams
+* [ffmpeg]https://ffmpeg.org/download.html#build-windows //For reading rtsp streams
+* npm i ws //For opening websockets
+* npm install dotenv --save //For reading .env
 
 ### Installing
 
 * Clone and leave directories as is.
+* You will need the .env file, you can request it from @Matthew Kastl or from IT.
 
 ### Executing program
 
--- server/index.js
+++ server/index.js
@@ -1,8 +1,41 @@
+/**
+ * The start point for the node.js webserver. The server serves the frontend
+ * for the HCP Surfcam Webserver and maintains and brodcasts the stream of the
+ * surf cam to all connections. 
+ *
+ * @summary Backend for the HCP Surfcam Webserver
+ * @author Matthew Kastl
+ * 
+ * Created at   :   2022-8-22
+ */
+ 
+/** jshint {inline configuration here} */
+
 const express = require('express');
+const Stream = require('node-rtsp-stream');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
+const STREAM_PORT = process.env.STREAM_PORT || 3002;
 
 app = express();
+stream = new Stream({
+    name: 'surfcam',
+    streamUrl: `rtsp://${process.env.AMCREST_USER}:${process.env.AMCREST_PASS}@166.203.202.132:554/cam/realmonitor?channel=1&subtype=0`,
+    wsPort: STREAM_PORT,
+    ffmpegOptions: {
+        '-stats': '',
+        '-r': 30
+    }
+});
+
+console.log(`rtsp://${process.env.AMCREST_USER}:${process.env.AMCREST_PASS}@166.203.202.132:554/cam/realmonitor?channel=1&subtype=0`);
+
+stream.on('exitWithError', () => {
+    stream.stop();
+    assert.fail('videoStream exited with error');
+    return done();
+  });
 
 app.get('/api', (req, res) => {
     res.json({message: "Hello from server!"});
