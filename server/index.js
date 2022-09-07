b06c604d3e12ee7841941bed79cdb21f9a4faaf3
Kastl <mkastl@islander.tamucc.edu>
2022-09-07 14:19:23 -0500

    General development on both front end and backend.

--- server/index.js
+++ server/index.js
@@ -10,36 +10,70 @@
  */
  
 
+//Webapp setup
 const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+const path = require('path');
+
+//Stream stuff
 const Stream = require('node-rtsp-stream');
 require('dotenv').config();
 
+
 const PORT = process.env.PORT || 3001;
 const STREAM_PORT = process.env.STREAM_PORT || 3002;
+var stream = null;
 
-app = express();
-stream = new Stream({
-    name: 'surfcam',
-    streamUrl: `rtsp://${process.env.AMCREST_USER}:${process.env.AMCREST_PASS}@166.203.202.132:554/cam/realmonitor?channel=1&subtype=0`,
-    wsPort: STREAM_PORT,
-    ffmpegOptions: {
-        '-stats': '',
-        '-r': 30
-    }
+function startStream(){
+    stream = new Stream({
+        name: 'surfcam',
+        streamUrl: `rtsp://${process.env.AMCREST_USER}:${process.env.AMCREST_PASS}@${process.env.HCP_IP}:${process.env.HCP_RTSP_PORT}/cam/realmonitor?channel=1&subtype=0`,
+        wsPort: STREAM_PORT,
+        ffmpegOptions: {
+            '-stats': '',
+            '-r': 30
+        }
+    });
+
+    stream.on('exitWithError', () => {
+        stream.stop();
+        assert.fail('videoStream exited with error');
+        return done();
+    });
+}
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+//serve files
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-console.log(`rtsp://${process.env.AMCREST_USER}:${process.env.AMCREST_PASS}@166.203.202.132:554/cam/realmonitor?channel=1&subtype=0`);
-
-stream.on('exitWithError', () => {
+app.get('/stop', (req, res) => {
+    console.log("Stop command recieved!");
     stream.stop();
-    assert.fail('videoStream exited with error');
-    return done();
-  });
-
-app.get('/api', (req, res) => {
-    res.json({message: "Hello from server!"});
 });
 
-app.listen(PORT, () => {
+app.get('/start', (req, res) => {
+    console.log("Start command recieved!");
+    startStream();
+});
+
+app.get('/test', (req, res) => {
+    stream.onSocketConnect();
+    //console.log(stream.stream);
+});
+
+io.on('connection', (socket) => {
+    console.log('====MY SOCKET HAS A CONNECTION====');
+});
+
+server.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
+startStream();
